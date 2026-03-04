acad14df9c8edf531ab75b6cf301c0c19a198141
Juan Pablo <walin@walin>
2026-03-04 09:38:19 -0500

    tests(suma): tests de suma completados

--- python_app/tests/test_lab.py
+++ python_app/tests/test_lab.py
@@ -21,3 +21,14 @@ def test_palindromo_python_false():
 def test_palindromo_radar_mayuscula_true():
     assert es_palindromo("Radar") == True  
 
+# Test para la función suma
+
+def test_suma_2_3():
+    assert suma(2, 3) == 5
+
+def test_suma_0_5():
+    assert suma(0, 5) == 5
+
+def test_suma_negativo_2_3():
+    assert suma(-2, 3) == 1
+
