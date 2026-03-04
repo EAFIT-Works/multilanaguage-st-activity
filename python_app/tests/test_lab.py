fceae61ccf23601b072012ef6e59e6f30f06cc7b
Juan Pablo <walin@walin>
2026-03-04 09:37:42 -0500

    tests(palindromo): tests de palindromos completados

--- python_app/tests/test_lab.py
+++ python_app/tests/test_lab.py
@@ -7,5 +7,17 @@
 from palindromo import es_palindromo
 from utils import suma
 
-def test_ejemplo_siembra():
-    assert True
+# Test para la función palindromo
+
+def test_palindromo_radar_true():
+    assert es_palindromo("radar") == True
+
+def test_palindromo_anita_true():
+    assert es_palindromo("anita lava la tina") == True
+
+def test_palindromo_python_false():
+    assert es_palindromo("python") == False
+
+def test_palindromo_radar_mayuscula_true():
+    assert es_palindromo("Radar") == True  
+
