33594ed2980c91cf10ab2d9c2f2ae79e0fd5fe7d
Juan Pablo <walin@walin>
2026-03-04 10:20:26 -0500

    tests(stringUtils.ts): se aplicaron todos los test para typescript

--- ts_app/tests/stringUtils.test.ts
+++ ts_app/tests/stringUtils.test.ts
@@ -1,11 +1,34 @@
-
-import { describe, it, expect } from 'vitest'
-import { isPalindrome } from '../src/stringUtils'
+import { describe, it, expect } from "vitest";
+import { isPalindrome } from "../src/stringUtils";
 
 // TODO: completa los casos: "radar"->true, "anita lava la tina"->true, "python"->false, ""->true, "Radar"->true
 
-describe('siembra', () => {
-  it('ejemplo', () => {
-    expect(true).toBe(true)
-  })
-})
+describe("isPalindrome", () => {
+  it("radar_es_palindromo", () => {
+    expect(isPalindrome("radar")).toBe(true);
+  });
+
+  it("anita_lava_la_tina_es_palindromo", () => {
+    expect(isPalindrome("anita lava la tina")).toBe(true);
+  });
+
+  it("anita_lava_la_tina_mayusculas_espacios_es_palindromo", () => {
+    expect(isPalindrome("aNi   ta laVa la Tina")).toBe(true);
+  });
+
+  it("python_no_es_palindromo", () => {
+    expect(isPalindrome("python")).toBe(false);
+  });
+
+  it("python_mayusculas_espacios_no_es_palindromo", () => {
+    expect(isPalindrome("pY thO       n")).toBe(false);
+  });
+
+  it("cadena_vacia_es_palindromo", () => {
+    expect(isPalindrome("")).toBe(true);
+  });
+
+  it("radar_mayuscula_es_palindromo", () => {
+    expect(isPalindrome("Radar")).toBe(true);
+  });
+});
