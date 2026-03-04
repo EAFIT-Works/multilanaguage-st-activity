bfc4b4329592b1471b42d662328164d86a1b4607
Juan Pablo <walin@walin>
2026-03-04 10:21:25 -0500

    fix(stringUtils.ts): se agregaron errores como el ==, codigo inalcanzable, console log, parametro inentendible y se mejoro la estructura del if

--- ts_app/src/stringUtils.ts
+++ ts_app/src/stringUtils.ts
@@ -2,13 +2,10 @@
 /**
  * Devuelve true si s es palíndromo (ignora espacios y mayúsculas/minúsculas).
  */
-export function isPalindrome(s: string): boolean {
-  let original = s; // variable no usada (intencional)
-  const cleaned = s.toLowerCase().replace(/\s+/g, '');
-  if (cleaned == cleaned.split('').reverse().join('')) { // eqeqeq (intencional)
+export function isPalindrome(string: string): boolean {
+  const cleaned = string.toLowerCase().replace(/\s+/g, '');
+  if (cleaned === cleaned.split('').reverse().join('')) {
     return true;
-  } else {
-    return false;
   }
-  console.log('Inalcanzable'); // unreachable (intencional)
+  return false;
 }
