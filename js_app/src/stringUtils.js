d9cdca5defc2142140a117e27870bed4ecc9193e
Juan Pablo <walin@walin>
2026-03-04 10:05:01 -0500

    fix(stringUtils): se le arregló el nombre del parametro a la funcion isPalindrome

--- js_app/src/stringUtils.js
+++ js_app/src/stringUtils.js
@@ -2,8 +2,8 @@
 /**
  * Devuelve true si s es palíndromo (ignora espacios y mayúsculas/minúsculas).
  */
-function isPalindrome(s) {
-  const cleaned = s.toLowerCase().replace(/\s+/g, '');
+function isPalindrome(string) {
+  const cleaned = string.toLowerCase().replace(/\s+/g, '');
   if (cleaned === cleaned.split('').reverse().join('')) {
     return true;
   }
