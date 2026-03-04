097c8dc0e5e5432ecd7c791d1d2e013f48d65b21
Juan Pablo <walin@walin>
2026-03-04 10:00:42 -0500

    tests(isPalindrome): se realizaron todos los casos de prueba pedidos para IsPalindrome

--- js_app/tests/stringUtils.test.js
+++ js_app/tests/stringUtils.test.js
@@ -1,7 +1,20 @@
-
 // TODO: Escribe pruebas unitarias para isPalindrome.
 // Sugerencias: "radar" -> true; "anita lava la tina" -> true; "python" -> false; "" -> true; "Radar" -> true
 
-test('ejemplo siembra', () => {
-  expect(true).toBe(true);
+const { isPalindrome } = require("../src/stringUtils");
+
+test("radar_es_palindromo", () => {
+  expect(isPalindrome("radar")).toBe(true);
+});
+
+test("anita_lava_la_tina_es_palindromo", () => {
+  expect(isPalindrome("anita lava la tina")).toBe(true);
+});
+
+test("python_no_es_palindromo", () => {
+  expect(isPalindrome("python")).toBe(false);
+});
+
+test("radar_mayuscula_es_palindromo", () => {
+  expect(isPalindrome("Radar")).toBe(true);
 });
